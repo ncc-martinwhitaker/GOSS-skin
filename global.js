110f824e43cc96bba73078ee1de75dc020dbfd7f
Kai-NCC <kajetan.orski@norfolk.gov.uk>
2024-11-20 13:49:01 +0000

    all sites - fade-titles intersection observer - 4934

--- global.js
+++ global.js
@@ -143,6 +143,25 @@ function disclosureListener() {
 	observer.observe(disc, { attributes: true });
 }
 
+// intersection observer
+
+const options = {
+	root: null,
+	rootMargin: "0px",
+	threshold: 0.25,
+};
+
+const callback = (entries, observer) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			let elem = entry.target; 
+			elem.classList.add("intersecting");
+		}
+	});
+  };
+
+const intersectionalObserver = new IntersectionObserver(callback, options);
+
 $(window).on("load", function () { 
 	if ($(".template--event")[0]){
 		museumsColour();
@@ -150,6 +169,13 @@ $(window).on("load", function () {
 	}
 	if ($(".template--eventsearch")[0]){
 		museumsSearchColour(); 
+	}
+	
+	if ($(".body--fade-titles")[0]){
+		const targets = document.querySelectorAll(".a-panel__content");
+		targets.forEach((panel) => {
+			intersectionalObserver.observe(panel);
+		});
 	}
 });
 
